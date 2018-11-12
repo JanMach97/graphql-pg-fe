d3c00ccfbe9c2f05dce6ed286be4a4b302fd2cb2
JanMach97 <janmachacek97@gmail.com>
2018-11-12 00:02:00 +0000

    add gql search query

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,6 +3,29 @@ import { withApollo } from "react-apollo";
 import gql from "graphql-tag";
 import Link from "./Link";
 
+const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filder: String!) {
+    feed(filter: $filter) {
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`;
+
 class Search extends Component {
   state = {
     links: [],
@@ -12,12 +35,11 @@ class Search extends Component {
     return (
       <div>
         <div>
-          Search
           <input
             type="text"
             onChange={e => this.setState({ filter: e.target.value })}
           />
-          <button onClick={() => this.executeSearch()}>OK</button>
+          <button onClick={() => this.executeSearch()}>Search</button>
         </div>
         {this.state.links.map((link, index) => (
           <Link key={link.id} link={link} index={index} />
